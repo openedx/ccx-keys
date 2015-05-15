f782eb489014d82162a1894c0d3017cdcad63c03
cewing <cris@crisewing.com>
2015-05-15 13:53:21 -0700

    the key will have a 'ccx' property

--- ccx_keys/key.py
+++ ccx_keys/key.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+from abc import abstractproperty
 from opaque_keys.edx.keys import CourseKey
 
 
 class CCXKey(CourseKey):
-    pass
+
+    @abstractproperty
+    def ccx(self):
+        """The id of the ccx for this key"""
+        raise NotImplementedError()
