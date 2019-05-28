48929c125cfb4f4c539ad8067cc875098ed13d11
Feanil Patel <feanil@edx.org>
2019-05-28 15:19:01 -0400

    Update classifiers.

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=[
         'ccx_keys',
